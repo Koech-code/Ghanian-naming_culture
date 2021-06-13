2920003b80aff6c93e238536fe4653bf3f73537f
Koech-code <nixon.koech@moringaschool.com>
2021-06-13 23:44:59 +0300

    made changes to the bootstrap documentation and made changes to the javascript code

--- Static/JS/main.js
+++ Static/JS/main.js
@@ -16,47 +16,46 @@ form.addEventListener("submit", function generate(event){
         alert("Your input for months field cannot be less than 0 and cannot be more than 12.");
     }
     else if (Math.round(result) == 0 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[0]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[0]);
     }
     else if (Math.round(result) == 1 || Math.round(result) == -1 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[1]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[1]);
     }
     else if (Math.round(result) == 2 || Math.round(result) == -2 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[2]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[2]);
     }
     else if (Math.round(result) == 3 || Math.round(result) == -3 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[3]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[3]);
     }
     else if (Math.round(result) == 4 || Math.round(result) == -4 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[4]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[4]);
     }
     else if (Math.round(result) == 5 || Math.round(result) == -5 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[5]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[5]);
     }
     else if (Math.round(result) == 6 || Math.round(result) == -6 && gender === "male"){
-        document.getElementById("output").innerHTML = ("Your Akan Name is " + Males[6]);
+        document.getElementById("output").innerHTML = ("Your Akan name is " + Males[6]);
     }
-
     else if (Math.round(result) == 0 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[0]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[0]);
     }
     else if (Math.round(result) == 1 || Math.round(result) == -1 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[1]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[1]);
     }
     else if (Math.round(result) == 2 || Math.round(result) == -2 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[2]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[2]);
     }
     else if (Math.round(result) == 3 || Math.round(result) == -3 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[3]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[3]);
     }
     else if (Math.round(result) == 4 || Math.round(result) == -4 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[4]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[4]);
     }
     else if (Math.round(result) == 5 || Math.round(result) == -5 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[5]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[5]);
     }
     else if (Math.round(result) == 6 || Math.round(result) == -6 && gender === "female"){
-    document.getElementById("output").innerHTML = ("Your Akan Name is " + Females[6]);
+    document.getElementById("output").innerHTML = ("Your Akan name is " + Females[6]);
     }
     else{
         alert("Fill in all the fields.");
